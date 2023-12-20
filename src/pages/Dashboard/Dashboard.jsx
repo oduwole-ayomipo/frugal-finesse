2971058b421391ff847bd1973009cd80274c8345
oduwole ayomipo <harkorede318@gmail.com>
2023-12-20 21:16:04 +0100

    Revert "feat: cards added to the dashboard ui, figures are rendered dynamically."
    This reverts commit 1560bd01d8867558b4f51405d2e64da5cdb0abb7.

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,6 @@
 import { useLocation } from "react-router-dom";
 import React from "react";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
-import CardOne from "../../components/Dashboard-Components/CardOne";
-import CardTwo from "../../components/Dashboard-Components/CardTwo";
 
 function Dashboard() {
   const location = useLocation();
@@ -24,23 +22,17 @@ function Dashboard() {
   }
 
   return (
-    <>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardOne Income={formatIncomeUI()} />
-        <CardTwo Income={formData.IncomeSetup} />
-        <CardOne Income={formatIncomeUI()} />
-        <CardOne Income={formatIncomeUI()} />
-      </div>
-      {/* <div className="flex">
-        <div className="m-24 mx-auto font-display text-lg font-semibold">
-          <Breadcrumbs pageName={"Dashboard"} />
-          <div>
-            <p>{formData.UsernameSetup}</p>
-            <p>{formData.BudgetSetup}</p>
-          </div>
+    <div className="flex">
+      <div className="m-24 mx-auto font-display text-lg font-semibold">
+        <Breadcrumbs pageName={"Dashboard"} />
+        <div>
+          <p>{formData.UsernameSetup}</p>
+          <p>{formatIncomeUI()}</p>
+          <p>{formData.BudgetSetup}</p>
         </div>
-      </div> */}
-    </>
+        Welcome to something that will eventually look like a dashboard!!!
+      </div>
+    </div>
   );
 }
 
