71d3c2440bfcb1ffaa3e3e8c0ea28596b91123b8
oduwole ayomipo <harkorede318@gmail.com>
2023-12-31 00:18:54 +0100

    feat: amount ui

--- src/components/data-tables/TransactionTable.jsx
+++ src/components/data-tables/TransactionTable.jsx
@@ -39,6 +39,16 @@ function TransactionTable({ openTransactionForm }) {
     }
   };
 
+  //convert the amount display
+  const formatAmount = () => {
+    if (initialTransaction && initialTransaction.amount) {
+      const formattedAmount = initialTransaction.amount.toLocaleString();
+      return formattedAmount;
+    } else {
+      return initialTransaction.amount;
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col gap-10">
@@ -77,7 +87,7 @@ function TransactionTable({ openTransactionForm }) {
                     </td>
                     <td className="border-b border-[#eee]  py-5 px-4 ">
                       <p className="text-purple-dark text-sm font-body">
-                        ₦{initialTransaction.amount}
+                        ₦{formatAmount()}
                       </p>
                     </td>
                     <td className="border-b border-[#eee] py-5 px-4">
@@ -123,15 +133,25 @@ function TransactionTable({ openTransactionForm }) {
                           Dec. 29, 2023
                         </p>
                       </td>
-                      <td className="border-b  text-sm font-body border-[#eee] py-5 px-4 ">
-                        <div className="flex items-center space-x-3.5">
-                          <button className="text-meta-1 font-medium hover:font-semibold">
-                            EDIT
-                          </button>
-                          <button className="text-meta-1 font-medium hover:font-semibold">
-                            DELETE
-                          </button>
-                        </div>
+                      <td className="border-b text-sm font-body border-[#eee] py-5 px-4 ">
+                        <button className="text-meta-1 w-full font-medium hover:font-semibold">
+                          <span className="w-full flex items-center justify-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="w-6 h-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                              />
+                            </svg>
+                          </span>
+                        </button>
                       </td>
                     </tr>
                   ))}
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -6,31 +6,22 @@ import { Outlet } from "react-router-dom";
 function Layout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  //verily, the layout checks the users collection in the db. if i exsits, display the outlets else display an alery for users to complete registration.. this then render the setup page
   return (
     <div className="bg-[#F9F6FF]">
-      {/* <!-- ===== Page Wrapper Start ===== --> */}
       <div className="flex h-screen overflow-hidden">
-        {/* <!-- ===== Sidebar Start ===== --> */}
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
-
         {/* <!-- ===== Content Area Start ===== --> */}
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* <!-- ===== Header Start ===== --> */}
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* <!-- ===== Header End ===== --> */}
-
-          {/* <!-- ===== Main Content Start ===== --> */}
           <main>
             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
               <Outlet />
             </div>
           </main>
-          {/* <!-- ===== Main Content End ===== --> */}
         </div>
         {/* <!-- ===== Content Area End ===== --> */}
       </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
     </div>
   );
 }
